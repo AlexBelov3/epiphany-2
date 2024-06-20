503a6f632c6a20a7aea215cbd74dee02ade11286
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-06-20 16:00:14 -0400

    Add files via upload

--- epiphany/data_loader_10kb.py
+++ epiphany/data_loader_10kb.py
@@ -93,7 +93,7 @@ class Chip2HiCDataset(torch.utils.data.Dataset):
 
             pad_X = np.zeros((X_chr.shape[0],self.seq_length*100+self.window_size - X_chr.shape[1])) #100
             X_chr = np.concatenate((X_chr, pad_X), axis=1)
-        if len(self.co_signals) >= index:
+        if len(self.co_signals) < index:
             if index == 0:
                 print("calculate the entire product")
                 X_chr_one_co_signal = np.outer(X_chr[0].astype('float32'), X_chr[0].astype('float32'))
@@ -108,5 +108,5 @@ class Chip2HiCDataset(torch.utils.data.Dataset):
                 X_chr_one_co_signal[:, -1] = new_prod
                 self.co_signals.append(X_chr_one_co_signal)
         co_signal = self.co_signals[index]
-        return X_chr.astype('float32'), y_chr.astype('float32'), X_chr.astype('float32'), X_chr.astype('float32')
+        return X_chr.astype('float32'), y_chr.astype('float32'), co_signal.astype('float32'), X_chr.astype('float32')
 
