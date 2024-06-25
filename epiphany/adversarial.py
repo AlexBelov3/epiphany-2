232cf9f6b4bfa5b0d697ab67fc44abfe66af2bf3
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-06-25 08:14:20 -0400

    Add files via upload

--- epiphany/adversarial.py
+++ epiphany/adversarial.py
@@ -300,15 +300,15 @@ def main():
             # loss = float(0.5)*mse_loss_up + float(0.5)*mse_loss_down
             initial_params = {name: param.clone() for name, param in new_model.named_parameters()}
             loss.backward()
-            # for name, param in new_model.named_parameters():
-            #     if param.grad is None:
-            #         print(f"No gradients for {name}")
+            for name, param in new_model.named_parameters():
+                if param.grad is None:
+                    print(f"No gradients for {name}")
 
             optimizer.step()
 
-            # for name, param in new_model.named_parameters():
-            #     if torch.equal(param, initial_params[name]):
-            #         print(f"Parameter {name} has NOT been updated.")
+            for name, param in new_model.named_parameters():
+                if torch.equal(param, initial_params[name]):
+                    print(f"Parameter {name} has NOT been updated.")
 
             # # Train discriminator
             # disc_optimizer.zero_grad()
