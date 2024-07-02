b2644fd647f1d83c4d06c40f48f5b9ff49b7e79d
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-02 09:45:15 -0400

    Add files via upload

--- epiphany/adversarial.py
+++ epiphany/adversarial.py
@@ -217,8 +217,8 @@ def main():
 
                         # y_hat_L_list.append(y_hat_L)
                         # y_hat_R_list.append(y_hat_R)
-                        y_hat_L_list.append(np.array(y_hat)[:100])
-                        y_hat_R_list.append(np.array(y_hat)[100:])
+                        y_hat_L_list.append(np.array(y_hat.cpu())[:100])
+                        y_hat_R_list.append(np.array(y_hat.cpu())[100:])
                         # y_hat_list.append(y_hat)
 
                         test_label_L, test_label_R = extract_diagonals(test_label.squeeze()) # ONLY LOOKING AT THE LEFT VECTOR
