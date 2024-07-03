a06bcbc7a0268ae0da06fc0dbf48d28fb5d54936
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-03 10:37:57 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -721,7 +721,7 @@ class branch_pbulk(nn.Module):
         self.classifier = nn.Sequential(
             nn.Linear(in_features=(400), out_features=512), #nn.Linear(in_features=(1936), out_features=512),
         )
-        self.classifier2 = nn.Sequential(nn.Linear(in_features=(512), out_features=200))
+        self.classifier2 = nn.Sequential(nn.Linear(in_features=(400), out_features=200)) #(nn.Linear(in_features=(512), out_features=200))
 
     def forward(self, x2):
 
@@ -731,7 +731,7 @@ class branch_pbulk(nn.Module):
         x4 = torch.cat((x3_2d, x2_2d), 1)
         x4 = self.total_extractor_2d(x4)
         x4 = torch.flatten(x4, 1)
-        x4 = self.classifier(x4)
+        x4 = self.classifier2(x4)
 
         return x4
 
