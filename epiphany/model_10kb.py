fae14c5beecee81bbb6fdef5824a3e02a7616fbe
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-08 11:00:32 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -118,11 +118,11 @@ class Net(nn.Module):
         self.input_channels = input_channels
         self.window_size = window_size
 
-        self.conv1 = ConvBlock(in_channels=self.input_channels, out_channels=70, kernel_width=17, stride=1, pool_size=4)
+        self.conv1 = ConvBlock(in_channels=self.input_channels, out_channels=70, kernel_width=17, stride=1, pool_size=0) #pool_size=4
         self.do1 = nn.Dropout(p=.1)
-        self.conv2 = ConvBlock(in_channels=70, out_channels=90, kernel_width=7, stride=1, pool_size=4)
+        self.conv2 = ConvBlock(in_channels=70, out_channels=90, kernel_width=7, stride=1, pool_size=0) #pool_size=4
         self.do2 = nn.Dropout(p=.1)
-        self.conv3 = ConvBlock(in_channels=90, out_channels=70, kernel_width=5, stride=1, pool_size=4)
+        self.conv3 = ConvBlock(in_channels=90, out_channels=70, kernel_width=5, stride=1, pool_size=0) #pool_size=4
         self.do3 = nn.Dropout(p=.1)
         self.conv4 = ConvBlock(in_channels=70, out_channels=50, kernel_width=5, stride=1)
         self.do4 = nn.Dropout(p=.1)
@@ -145,7 +145,8 @@ class Net(nn.Module):
         x = x.squeeze()
         assert x.shape[0] == self.input_channels, f"Expected {self.input_channels} input channels, but got {x.shape[0]}"
         # x = torch.as_strided(x, (seq_length, self.input_channels, self.window_size), (100, x.shape[1], 1))
-        x = torch.as_strided(x, (seq_length, self.input_channels, self.window_size), (1, x.shape[1], 1))
+        # x = torch.as_strided(x, (seq_length, self.input_channels, self.window_size), (1, x.shape[1], 1))
+        x = x.unsqueeze(0)
         print(f"x.shape: {x.shape}")
         x = self.conv1(x)
         x = self.do1(x)
