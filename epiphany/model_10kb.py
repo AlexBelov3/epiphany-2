d99e91ccdaba7ad770d03de9efecd2444c233e6c
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-08 11:33:02 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -187,6 +187,7 @@ class Net(nn.Module):
         # x = self.conv8(x)
         # x = self.do8(x)
         # print(f"x output shape: {x.shape}")
+        x.squeeze()
         x_R, x_L = extract_diagonals(x)
         x = torch.concat(x_R, x_L)
         return x#, hidden_state
