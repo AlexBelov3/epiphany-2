6adf024602fe10c483371dbeeeba96b16b041db2
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-08 10:02:14 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -118,16 +118,6 @@ class Net(nn.Module):
         self.input_channels = input_channels
         self.window_size = window_size
 
-        # self.conv1 = ConvBlock(in_channels=self.input_channels, out_channels=70, kernel_width=17, stride=1, pool_size=4)
-        # self.do1 = nn.Dropout(p=.1)
-        # self.conv2 = ConvBlock(in_channels=70, out_channels=90, kernel_width=7, stride=1, pool_size=4)
-        # self.do2 = nn.Dropout(p=.1)
-        # self.conv3 = ConvBlock(in_channels=90, out_channels=70, kernel_width=5, stride=1, pool_size=4)
-        # self.do3 = nn.Dropout(p=.1)
-        # self.conv4 = ConvBlock(in_channels=70, out_channels=20, kernel_width=5, stride=1)
-        # self.pool = nn.AdaptiveMaxPool1d(900 // 20)
-        # self.do4 = nn.Dropout(p=.1)
-
         self.conv1 = ConvBlock(in_channels=self.input_channels, out_channels=70, kernel_width=17, stride=1, pool_size=4)
         self.do1 = nn.Dropout(p=.1)
         self.conv2 = ConvBlock(in_channels=70, out_channels=90, kernel_width=7, stride=1, pool_size=4)
@@ -137,7 +127,7 @@ class Net(nn.Module):
         self.conv4 = ConvBlock(in_channels=70, out_channels=50, kernel_width=5, stride=1)
         self.do4 = nn.Dropout(p=.1)
         self.conv5 = ConvBlock(in_channels=50, out_channels=20, kernel_width=5, stride=1)
-        self.pool = nn.AdaptiveMaxPool1d(200 // 20) #900 // 20
+        self.pool = nn.AdaptiveMaxPool1d(900 // 20) #900 // 20
         self.do5 = nn.Dropout(p=.1)
 
         self.rnn1 = nn.LSTM(input_size=900, hidden_size=1200, num_layers=num_layers, batch_first=True, bidirectional=True)
