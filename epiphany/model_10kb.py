df7ec5ed5f322e06cf689bc1b803c20ddc9284b1
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-11 10:17:52 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -806,9 +806,7 @@ class branch_outer_prod_big(nn.Module):
         x3_2d = self.bulk_summed_2d(x2)
         x4 = self.total_extractor_2d(x3_2d)
         x4 = torch.flatten(x4, 1)
-        print(f"x4 shape: {x4.shape}")
         x4 = self.classifier2(x4)
-        print(f"x4 shape: {x4.shape}")
         return x4
 
 class branch_pbulk(nn.Module):
