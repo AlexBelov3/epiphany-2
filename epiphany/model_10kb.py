cf4a6e46ab59a6eff359dcd9801fe69f1e5085d8
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-11 15:19:27 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -1198,13 +1198,13 @@ class branch_cov_2d(nn.Module):
             ),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Conv2d(
                 in_channels=16, out_channels=16, kernel_size=(1,5), stride=1, padding=(1,2)
             ),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Conv2d(
                 in_channels=16,
                 out_channels=16,
@@ -1215,10 +1215,10 @@ class branch_cov_2d(nn.Module):
             ),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=(1,2)),
             resblock_2d(16),
             resblock_2d(16),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Conv2d(
                 in_channels=16,
                 out_channels=16,
@@ -1229,7 +1229,7 @@ class branch_cov_2d(nn.Module):
             ),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Conv2d(
                 in_channels=16,
                 out_channels=16,
@@ -1240,7 +1240,7 @@ class branch_cov_2d(nn.Module):
             ),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=(1,2)),
             nn.Conv2d(
                 in_channels=16,
                 out_channels=1, #16
