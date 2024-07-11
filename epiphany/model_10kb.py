6fcde811c2ae3012dcf0a1c1b467656bb6675320
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-11 16:33:35 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -1267,7 +1267,7 @@ class branch_cov_2d(nn.Module):
             x = x.view(1, a, b, c)
         # print(f"input shape: {x.shape}")
         x = self.cov_extractor(x)
-        print(f"x shape: {x.shape}")
+        # print(f"x shape: {x.shape}")
         # x = torch.flatten(x, 1)
         x_out = self.classifier(x)
 
