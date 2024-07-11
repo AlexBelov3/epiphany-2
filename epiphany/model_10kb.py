d26bc03447fc9736d0355d9872d59b232e8d39b5
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-11 15:48:02 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -1215,8 +1215,8 @@ class branch_cov_2d(nn.Module):
             ),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,2)),
-            resblock_2d(16),
+            # nn.MaxPool2d(kernel_size=(1,2)),
+            # resblock_2d(16),
             # resblock_2d(16),
             # nn.MaxPool2d(kernel_size=(1,2)),
             # nn.Conv2d(
