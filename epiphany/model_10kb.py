206f784e556942827144040ff98d0c09f27fe650
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-12 10:38:36 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -811,11 +811,7 @@ class branch_outer_prod_small(nn.Module):
 class branch_outer_prod_high_res(nn.Module):
     def __init__(self):
         super(branch_outer_prod_high_res, self).__init__()
-        pbulk_res = 100
-        # 50 = 220
-        # 100 = 440
-        # 1kb = 4,400
-        # 10kb = 44,000
+        pbulk_res = 150
 
         self.bulk_summed_2d = nn.Sequential(
             nn.AvgPool1d(kernel_size=np.int64(1e04 / pbulk_res)), outer_prod()
