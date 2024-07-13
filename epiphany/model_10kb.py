7bca53cee11db689c8c017f8007200c89999d8fa
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-13 18:21:46 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -828,9 +828,8 @@ class branch_transformer(nn.Module):
         self.bulk_summed = nn.Sequential(
             nn.AvgPool1d(kernel_size=np.int64(1e04 / pbulk_res))
         )
-        self.transformer = nn.Transformer(d_model=440, nhead=8, num_encoder_layers=6, num_decoder_layers=6,
-                                          dim_feedforward=2048, dropout=0.1, activation='relu',
-                                          layer_norm_eps=1e-05, batch_first=True)
+        self.transformer = nn.Transformer(d_model=440, nhead=8, num_encoder_layers=3, num_decoder_layers=3,
+                                          dim_feedforward=2048, dropout=0.1, activation='relu', batch_first=True)
         self.fc1 = nn.Linear(440 * 5, 512)  # 5 is the sequence length
         self.fc2 = nn.Linear(512, 200)
         # self.fc1 = nn.Linear(2400, 900)
