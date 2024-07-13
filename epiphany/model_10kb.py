f058075333a6f178c682426f3e482559c0e0b6d1
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-13 18:49:19 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -839,9 +839,7 @@ class branch_transformer(nn.Module):
 
     def forward(self, x2):
         x = self.bulk_summed(x2)
-        print(f"input shape: {x.shape}")
         x = self.transformer(x, x)
-        print(f"transformer output shape: {x.shape}")
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.fc2(x)
