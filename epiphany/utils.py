d17382d0589d6d19c70fe919992bf83e86545dd6
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-15 20:47:31 -0400

    Add files via upload

--- epiphany/utils.py
+++ epiphany/utils.py
@@ -403,15 +403,15 @@ def generate_hic_test(label, y_up_list, y_down_list, path='./', seq_length=200):
             if i + j < seq_length:  # Ensure within bounds
                 if im[99 - j, i + j] == 0:
                     im[99 - j, i + j] = diag_values_down[j]
-                else:
-                    im[99 - j, i + j] = (diag_values_down[j] + im[99 - j, i + j]) / 2
+                # else:
+                #     im[99 - j, i + j] = (diag_values_down[j] + im[99 - j, i + j]) / 2
 
             # Diagonal Up (Left)
             if i - j >= 0:  # Ensure within bounds
                 if im[99 - j, i - j] == 0:
                     im[99 - j, i - j] = diag_values_up[j]
-                else:
-                    im[99 - j, i - j] = (diag_values_up[j] + im[99 - j, i - j]) / 2
+                # else:
+                #     im[99 - j, i - j] = (diag_values_up[j] + im[99 - j, i - j]) / 2
 
     # Save and plot the image
     path = os.path.join(path, 'ex_test.png')
