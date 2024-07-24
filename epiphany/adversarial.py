21c38fd1a9c03789d5c50921cdfe3f639035d57e
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-24 11:45:31 -0400

    Add files via upload

--- epiphany/adversarial.py
+++ epiphany/adversarial.py
@@ -193,7 +193,7 @@ def main():
                         y_hat = model(test_data)
                     for j in range(NUM_Vs):
                         y_hat_L_list.append(torch.tensor(np.array(y_hat.cpu())[j][:100]))
-                        y_hat_R_list.append(torch.tensor(np.array(y_hat.cpu())[j][100:])))
+                        y_hat_R_list.append(torch.tensor(np.array(y_hat.cpu())[j][100:]))
                     # y_hat_L_list.append(torch.tensor(np.array(y_hat.cpu())[0][:100]))
                     # y_hat_R_list.append(torch.tensor(np.array(y_hat.cpu())[0][100:]))
 
