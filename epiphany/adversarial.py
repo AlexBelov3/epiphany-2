898bd17a339b13867f7910ac4e1290f05b61fb27
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-25 10:31:03 -0400

    Add files via upload

--- epiphany/adversarial.py
+++ epiphany/adversarial.py
@@ -188,10 +188,14 @@ def main():
                 test_data, test_label = torch.Tensor(test_data).cuda(), torch.Tensor(test_label).cuda()
                 with torch.no_grad():
                     y_hat = model(test_data)#.squeeze()
-                    print(f"y_hat shape: {y_hat.shape}")
-                    for j in range(NUM_Vs):
-                        y_hat_L_list.append(torch.tensor(np.array(y_hat.cpu())[:100]))
-                        y_hat_R_list.append(torch.tensor(np.array(y_hat.cpu())[100:]))
+                    if NUM_Vs == 1:
+                        for j in range(NUM_Vs):
+                            y_hat_L_list.append(torch.tensor(np.array(y_hat.cpu())[:100]))
+                            y_hat_R_list.append(torch.tensor(np.array(y_hat.cpu())[100:]))
+                    else:
+                        for j in range(NUM_Vs):
+                            y_hat_L_list.append(torch.tensor(np.array(y_hat.cpu()[0])[:100]))
+                            y_hat_R_list.append(torch.tensor(np.array(y_hat.cpu()[0])[100:]))
                     # y_hat_L_list.append(torch.tensor(np.array(y_hat.cpu())[0][:100]))
                     # y_hat_R_list.append(torch.tensor(np.array(y_hat.cpu())[0][100:]))
 
