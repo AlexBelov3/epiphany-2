88ae507082151b0a2ecd6e9aa8bc66fe2e22757f
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-25 13:26:43 -0400

    Add files via upload

--- epiphany/utils.py
+++ epiphany/utils.py
@@ -379,6 +379,7 @@ def generate_image_test(label, y_up_list, y_down_list, path='./', seq_length=200
 
     bands = len(label)
     print(f"bands: {bands}")
+    print(label)
     label = np.flip(label, axis=0)
     for j in range(bands - 1):
         if j > 0:
