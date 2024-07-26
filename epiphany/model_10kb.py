4109c7657d2258acd4d5cf7d4576682c5391b046
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-26 15:15:44 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -1888,7 +1888,7 @@ class branch_small_pbulk_prod(nn.Module):
             ),
             nn.BatchNorm1d(16), #1
             nn.ReLU(),
-            nn.Linear(in_features=(343), out_features=220),
+            # nn.Linear(in_features=(343), out_features=220),
             outer_prod(),
         )
 
