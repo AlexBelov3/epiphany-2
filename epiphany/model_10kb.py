d3cf1438bee88c2382b7cbef72c2feb313555931
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-26 15:28:42 -0400

    Add files via upload

--- epiphany/model_10kb.py
+++ epiphany/model_10kb.py
@@ -1893,7 +1893,8 @@ class branch_small_pbulk_prod(nn.Module):
         )
 
         self.total_extractor_2d = nn.Sequential(
-            nn.Conv2d(in_channels=21, out_channels=64, kernel_size=3, stride=2), #nn.Conv2d(in_channels=36, out_channels=64, kernel_size=3, stride=2),
+            nn.Conv2d(in_channels=21, out_channels=64, kernel_size=3, stride=2),
+            # nn.Conv2d(in_channels=36, out_channels=64, kernel_size=3, stride=2),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
