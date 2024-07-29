4e3c999f038979d350317333d5ef5b1c44a7802c
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-29 10:22:32 -0400

    Add files via upload

--- epiphany/utils.py
+++ epiphany/utils.py
@@ -348,7 +348,10 @@ def generate_image_test(label, y_up_list, y_down_list, path='./', seq_length=200
     # Initialize the image arrays
     im1 = np.zeros((seq_length, seq_length))
     im2 = np.zeros((seq_length, seq_length))
-
+    print(f"len label: {len(label)}")
+    print(f"len y_up_list: {len(y_up_list)}")
+    print(f"len y_down_list: {len(y_down_list)}")
+    print(f"seq_length: {seq_length}")
     # Fill the top diagonal with the reconstructed Hi-C from label diagonals
     for i in range(seq_length):
         diag_values_down = y_down_list[i].cpu()
