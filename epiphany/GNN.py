a612cd4aca5298d2c96e26cdee282823e4289e98
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-30 11:54:56 -0400

    Add files via upload

--- epiphany/GNN.py
+++ epiphany/GNN.py
@@ -78,7 +78,7 @@ class EdgeWeightMPNN(MessagePassing):
 # Parameters for the dataset
 window_size = 14000
 chroms = ['chr17']
-save_dir = 'Epiphany_dataset'
+save_dir = '/data/leslie/belova1//Epiphany_dataset'
 
 # Create instances of the custom dataset
 train_dataset = GraphDataset(window_size=window_size, chroms=chroms, save_dir=save_dir)
