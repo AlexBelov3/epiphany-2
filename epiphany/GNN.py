1e08e7255ecdee8b9d66130ce3d49c2b7d2c1a2f
AlexBelov3 <141083742+AlexBelov3@users.noreply.github.com>
2024-07-31 10:16:04 -0400

    Add files via upload

--- epiphany/GNN.py
+++ epiphany/GNN.py
@@ -62,8 +62,8 @@ class EdgeWeightMPNN(MessagePassing):
             nn.Conv1d(in_channels=32, out_channels=16, kernel_size=3, padding='same'),
             nn.BatchNorm1d(16),
             nn.ReLU(),
-            nn.Conv1d(in_channels=16, out_channels=16, kernel_size=3, padding='same'),
-            nn.BatchNorm1d(16),
+            nn.Conv1d(in_channels=16, out_channels=1, kernel_size=3, padding='same'),
+            nn.BatchNorm1d(1),
             nn.ReLU(),
         )
         self.linear = nn.Linear(16 * (track_length // 8) + 1, hidden_dim)
